5d3bf5de87056ea93afd28e61bb449208437e77d
Muhammad Firdaus <mbex71@gmail.com>
2021-12-10 18:03:05 +0700

    branch dev

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,25 +1,18 @@
 import dynamic from 'next/dynamic'
 import { lazy, Suspense } from 'react';
-import Coba from '../components/Title'
+// import Coba from '../components/Title'
 
-// const AppSaya = dynamic(
-//     () => import('app2/Title'),
-//     { ssr: false ,
-//     loading: ({error, isLoading}) => <div>{isLoading ? 'LOADING!!! ' : null} </div>
-//     }
-// )
+const AppSaya = dynamic(
+    () => import('app2/Title'),
+    { ssr: false ,
+    loading: () => <div>Loading . . .</div>
+    }
+)
 
-
-// const Test = () => (<Suspense fallback="null"><AppSaya /></Suspense>)
 const Test = () => {
     return (
         <>
-        <div>Hallo</div>
-        {/* <AppSaya/> */}
-        <Coba />
-        {/* <Suspense fallback="Loading . . .">
-
-        </Suspense> */}
+        <AppSaya/>
         </>
     )
 }
